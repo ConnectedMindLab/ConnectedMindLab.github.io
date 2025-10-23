1c3f2c4cd1197c5aacfec24a4689bfb174ef6513
ConnectedMindLab <jeongeun12@snu.ac.kr>
2025-10-23 09:59:41 +0000

    Deploying to gh-pages from @ ConnectedMindLab/ConnectedMindLab.github.io@ea92d61abd6d40a793d846589492cfb85bb8555c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,14 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/fullbattery/";
               },
-            },{id: "nav-blog",
+            },{id: "nav-구성원들",
+          title: "구성원들",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
+        },{id: "nav-blog",
           title: "Blog",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
